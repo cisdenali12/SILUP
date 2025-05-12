d4b8531bb72a795d47ee7de409420ca4e86c248c
mariomacu12 <mariomacu12@gmail.com>
2025-05-12 13:24:11 -0700

    Update main.jsx
    limpiando el codigo

--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import { RouterProvider } from "react-router-dom";
+import { createRouter } from './router'
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createNavigation } from './router'
+
 import './index.css'
 
-import App from './App.jsx'
 
-import {HomePage} from "./pages/HomePage";
-
-import {RegisterPage} from "./pages/RegisterPage";
-
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App />,        //  layout
-//     children: [
-//       { index: true,     element: <HomePage /> },   
-//       { path: "register", element: <RegisterPage /> },   
-//       { path: "categories, element: <CategoryPage /> },   
-//     ],
-//   },
-// ]);
-
-
-
-createNavigation(15)
+const router = createRouter()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <RouterProvider router={router} /> */}
+    <RouterProvider router={router} />
   </StrictMode>
 );
 
