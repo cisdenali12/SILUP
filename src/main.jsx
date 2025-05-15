59e276bffa6f38e75c58f9f862fd23e906204a3b
Roberto Langarica <roberto.langarica@gmail.com>
2025-05-14 21:25:50 -0700

    Providing the store for all the app

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { createRouter } from './router'
 import { StrictMode } from 'react'
+import  { store } from './store'
+import { Provider } from 'react-redux'
 
 import './index.css'
 
@@ -11,7 +13,9 @@ const router = createRouter()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
 
