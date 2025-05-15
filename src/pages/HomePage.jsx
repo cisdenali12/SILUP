ccab86a9c847f38a2c6b338f3b025d610e4654fe
Roberto Langarica <roberto.langarica@gmail.com>
2025-05-14 21:27:27 -0700

    Adding the login flow with the error message handling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,38 @@
-import React from "react";
-import {LoginForm} from '../components/LoginForm';
+import React, { useEffect } from "react";
+import { LoginForm } from '../components/LoginForm';
+import { MessagePopup } from '../components/popups';
+import  { userActions, userSelectors } from '../store'
+import { useDispatch } from "react-redux";
 
 export function HomePage() {
+  // TODO: Agregar un estado de cargando para toda la pantalla
+  const showErrorPopup = userSelectors.loginError()
+  const isLoggedIn = userSelectors.isLoggedIn()
+
+  const dispatch = useDispatch()
+
+  const handleSubmit = ({email, password})=>{
+    dispatch(userActions.login({email, password}))
+  }
+
+  const handleCloseError = ()=>{
+    dispatch(userActions.loginNoError())
+  }
+
+  useEffect(()=>{
+    console.log('EFECTO', isLoggedIn)
+
+    if(isLoggedIn){
+      console.log('Navigate forward!!')
+      // TODO: Agregar navegacion justo despues de que el usuario hace login
+    }
+  }, [isLoggedIn])
+
   return (
-  
-  <div className=" flex-flow" >
-  <h1 className="text-red-900">Bienvenido a SILUP <br /> tu inventario personal </h1>
-
-  <LoginForm />
-
+  <div className="grid grod-cols-1 grid-rows-[auto 1fr] h-screen">
+    <span className="text-red-900 row-1">Bienvenido a SILUP </span>
+    <LoginForm className="row-2" onSubmit={ handleSubmit } />
+    <MessagePopup  type='error' show={ showErrorPopup } onClose={ handleCloseError }/>
   </div>
   )
 }
