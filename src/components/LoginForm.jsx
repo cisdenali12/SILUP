60b6a87bcba9656351da0567119ca066376516a0
Roberto Langarica <roberto.langarica@gmail.com>
2025-05-15 18:48:38 -0700

    Some better layout

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -10,13 +10,18 @@ export function LoginForm ({className, onSubmit = console.log}){
 
 return (
     <div className={`${className} h-full`}>
-      <form action={ handleSubmit }>
-          <label htmlFor="mail">Correo electrónico:</label>
-          <input type="email" id="mail" name="user_mail" />
-          <label htmlFor="pwd">Contraseña:</label>
-          <input type="password" id="pwd" name="pwd" />
-          <button  type="submit" />
-      
+      <form action={ handleSubmit } className="h-full grid grid-cols-1 text-black">
+          <span className="row-1 flex flex-col-reverse pl-2">
+            <label className="text-left flex-none" htmlFor="mail">Correo electrónico:</label>
+          </span>
+          <input className="row-2 bg-blue-50 pl-1" type="email" id="mail" name="user_mail" placeholder="email" />
+          
+          <span className="row-3 flex flex-col-reverse pl-2">
+            <label className="text-left flex-none" htmlFor="pwd">Contraseña:</label>
+          </span>
+          <input className="row-4 bg-blue-50 pl-1" type="password" id="pwd" name="pwd" placeholder="contraseña"/>
+          
+          <button className="row-6 bg-blue-400 text-white"  type="submit">Login</button>
       </form>
   </div>
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,9 +3,12 @@ import { LoginForm } from '../components/LoginForm';
 import { MessagePopup } from '../components/popups';
 import  { userActions, userSelectors } from '../store'
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
+import { RouteNames } from '../router'
 
 export function HomePage() {
   // TODO: Agregar un estado de cargando para toda la pantalla
+  const navigate = useNavigate()
   const showErrorPopup = userSelectors.loginError()
   const isLoggedIn = userSelectors.isLoggedIn()
 
@@ -19,19 +22,25 @@ export function HomePage() {
     dispatch(userActions.loginNoError())
   }
 
-  useEffect(()=>{
-    console.log('EFECTO', isLoggedIn)
+  const handleRegister = ()=>{
+    navigate(RouteNames.REGISTER, {replace:true})
+  }
 
+  useEffect(()=>{
     if(isLoggedIn){
       console.log('Navigate forward!!')
-      // TODO: Agregar navegacion justo despues de que el usuario hace login
+      navigate(RouteNames.DASHBOARD)
     }
   }, [isLoggedIn])
 
   return (
-  <div className="grid grid-rows-[70px_1fr] grid-cols-1 h-screen">
-    <span className="text-red-900 row-1">Bienvenido a SILUP </span>
-    <LoginForm className="row-2" onSubmit={ handleSubmit } />
+  <div className="grid grid-cols-1 h-screen p-5">
+    <span className="row-1 text-red-900 text-9xl font-black">SILUP </span>
+    <span className="row-2 text-red-900 text-xl">Bienvenido! Ingresa a tu cuenta</span>
+    <LoginForm className="row-3" onSubmit={ handleSubmit } />
+    <a className="row-4 grid grid-rows-1" href="">
+      <span className="row-2 underline text-sm hover:no-underline" onClick={handleRegister}>No tienes cuenta? Registrate aqui</span>
+    </a>
     <MessagePopup  type='error' show={ showErrorPopup } onClose={ handleCloseError }/>
   </div>
   )
