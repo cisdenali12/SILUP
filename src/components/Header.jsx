35817d24a578cc4655c50a76a91982445b56994b
Roberto Langarica <roberto.langarica@gmail.com>
2025-05-20 21:13:31 -0700

    Connecting the header to the store and fixing the style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,24 @@
 import React from "react";
-import "./Header.css" ;
-// export function Header() {
-// return (
-// <div className="header">hola</div>
-// )
-// }
+import { useSelector } from "react-redux";
 
-export default function () {
+export function Header () {
+    const user = useSelector((s)=>`${s.user.name} <${s.user.email}>`)
+
+    const Logo = ({className})=>{
+        return (
+            <div className={`${className} flex items-center rounded-r-full`}>
+                <span className="text-red-900 bg-white font-black text-2xl mx-3">
+                    SILUP
+                </span>
+            </div>
+        )
+    }
+
 return (
-<div className="Header">SILUP</div>
+    <div className="row-[header] flex items-center justify-between bg-red-900">
+        <Logo className="bg-white h-full"/>
+        <span className="mr-3">{user}</span>
+    </div>
 )
 }
 
